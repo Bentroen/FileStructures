2fd5c907f3c1046721b8b99e072996258c69f3c1
Bentroen <bemcdc@gmail.com>
2021-12-14 18:54:47 -0300

    Modifica script para buscar por substring no logradouro

--- aula3_busca_logradouro/busca.py
+++ aula3_busca_logradouro/busca.py
@@ -2,22 +2,21 @@ import struct
 import sys
 
 if len(sys.argv) != 2:
-	print ("USO python3 {} [CEP]".format(sys.argv[0]))
+	print ("Uso: python3 {} <logradouro>".format(sys.argv[0]))
 	quit()
 
 
 registroCEP = struct.Struct("72s72s72s72s2s8s2s")
-cepColumn = 5
+addressColumn = 0
 print ("Tamanho da Estrutura: {}".format(registroCEP.size))
 with open("cep.dat","rb") as f:
 	line = f.read(registroCEP.size)
 	counter = 0
 	while len(line) > 0:
 		record = registroCEP.unpack(line)
-		if record[cepColumn].decode('latin1') == sys.argv[1]:
+		if sys.argv[1] in record[addressColumn].decode('latin1'):
 			for i in range(0,len(record)-1):
 				print (record[i].decode('latin1'))
-			break
 		line = f.read(registroCEP.size)
 		counter += 1
 	print ("Total de Registros Lidos: {}".format(counter))
